b0f96889d150bbb432078bfa3c8b1429a358ca04
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-20 14:42:59 +0300

    create a .json file with JSON data and used a server to get the data

--- Tuts/dojo-blog/src/Home.js
+++ Tuts/dojo-blog/src/Home.js
@@ -10,8 +10,6 @@ const Home = () => {
         { title: 'My world map', body: 'loream ipsum...', author: 'Nami', id:3 }
     ]);
 
-    const [name, setName] = useState('Usopp');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -19,14 +17,11 @@ const Home = () => {
 
     useEffect(() => {
         console.log('Use effect ran');
-        console.log(name)
-    }, [name]);
+    }, []);
 
     return (  
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <button onClick={() => setName('Chopper')}>Change Name</button>
-            <p>{ name }</p>
         </div>
     );
 }
