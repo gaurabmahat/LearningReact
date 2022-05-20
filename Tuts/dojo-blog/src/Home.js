0b7d81d952921e8c771f977fb161e576ea4e9321
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-20 11:46:53 +0300

    adding props and using the props properties in different ways

--- Tuts/dojo-blog/src/Home.js
+++ Tuts/dojo-blog/src/Home.js
@@ -1,5 +1,5 @@
-import { logRoles } from "@testing-library/react";
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
@@ -12,12 +12,7 @@ const Home = () => {
 
     return (  
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title } </h2>
-                    <p>Written by { blog.author } </p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!" />
         </div>
     );
 }
