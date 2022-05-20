b9f4f0b9860c033fe6eaabad15cdf2e332593515
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-20 10:29:42 +0300

    different click events

--- Tuts/dojo-blog/src/Home.js
+++ Tuts/dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello, my subordinates');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('Hello ' + name);
+    }
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {
+                handleClickAgain('Chopper')
+            }}>Click Me Again</button>
         </div>
     );
 }
