8f6a75cd9e7eaa6a1e5e165f5ec3d64ab13cdade
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-20 14:54:43 +0300

    fetching data from server and displaying it in the web browser

--- Tuts/dojo-blog/src/Home.js
+++ Tuts/dojo-blog/src/Home.js
@@ -4,24 +4,21 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     //let name = 'Usopp';
-    const [blogs, setBlogs] = useState([
-        { title: 'My new adventure', body: 'loream ipsum...', author: 'Usopp', id:1 },
-        { title: 'Welcome my patient', body: 'loream ipsum...', author: 'Chopper', id:2 },
-        { title: 'My world map', body: 'loream ipsum...', author: 'Nami', id:3 }
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('Use effect ran');
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+        })
+        .then((data) => {
+            setBlogs(data);
+        })
     }, []);
 
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
