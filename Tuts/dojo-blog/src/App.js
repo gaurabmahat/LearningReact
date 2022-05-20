9de5f5fd3fe22831539445684a3dced4d0dafbf6
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-20 09:24:36 +0300

    examples of how to output dynamic values

--- Tuts/dojo-blog/src/App.js
+++ Tuts/dojo-blog/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const like = 50;
+
+  //We cannot output Object or boolean. For ex: we cannot output
+  //the person Object created below.
+  //const person = { name: 'Usopp', age: 20 };
+
+  const link = "https://duckduckgo.com/";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { like } times</p>
+
+        {/*Cannot ouput the person Object. */}
+        {/* <p> { person } </p> */}
+
+        {/*We can output all of the value types as stated
+        below. */}
+        <p>{ 10 } </p>
+        <p>{ "Hello, Usopp"}</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }> DuckDuckGo Site</a>
+
       </div>
     </div>
   );
