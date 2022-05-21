21a30e4dfe335f06bd333f9a5b483c769d7cf695
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-21 13:17:09 +0300

    created forms and ways to take the values of the form

--- Tuts/dojo-blog/src/Create.js
+++ Tuts/dojo-blog/src/Create.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
-    return (  
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Usopp');
+
+    return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Usopp">Usopp</option>
+                    <option value="Chopper">Chopper</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
- 
+
 export default Create;
