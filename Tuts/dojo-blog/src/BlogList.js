aa2cf8607b9c2bb809f74c35d6de7561802a3dbf
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-21 11:31:45 +0300

    added a BlogDetails file and updated BlogList such that we can navigate to the blog by clicking it

--- Tuts/dojo-blog/src/BlogList.js
+++ Tuts/dojo-blog/src/BlogList.js
@@ -1,4 +1,6 @@
-const BlogList = (/*props*/ {blogs, title}) => {
+import { Link } from "react-router-dom";
+
+const BlogList = (/*props*/ { blogs, title }) => {
     // const blogs = props.blogs;
     // const title = props.title;
 
@@ -7,8 +9,10 @@ const BlogList = (/*props*/ {blogs, title}) => {
             <h2> {title} </h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title} </h2>
-                    <p>Written by {blog.author} </p>
+                    <Link to={ `/blogs/${ blog.id }`}>
+                        <h2>{blog.title} </h2>
+                        <p>Written by {blog.author} </p>
+                    </Link>
                 </div>
             ))}
         </div>
